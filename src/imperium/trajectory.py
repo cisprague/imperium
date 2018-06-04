bce14c6d5619b0954d0e106625c9233c1edf24f4
cisprague <christopher.iliffe.sprague@gmail.com>
2018-06-04 16:49:58 +0200

    Home

---
+++ src/imperium/trajectory.py
@@ -0,0 +1,15 @@
+# Christopher Iliffe Sprague
+# christopher.iliffe.sprague@gmail.com
+
+class Trajectory(object):
+
+    def __init__(self, segments):
+
+        # segments
+        self.segments = segments
+
+class Indirect(Trajectory):
+
+    def __init__(self, segments):
+
+        pass
