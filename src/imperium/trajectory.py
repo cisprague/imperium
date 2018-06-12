c269db1757aadc87286f1f2dcc3ae6b0caf04596
cisprague <christopher.iliffe.sprague@gmail.com>
2018-06-12 08:40:49 +0200

    work

--- src/imperium/trajectory.py
+++ src/imperium/trajectory.py
@@ -2,7 +2,7 @@
 # christopher.iliffe.sprague@gmail.com
 
 import numpy as np
-from imperium.segment import Indirect
+import imperium.segment as segment
 
 class Trajectory(object):
 
@@ -11,6 +11,17 @@ class Trajectory(object):
         # segments
         self.dynamics = dynamics
 
+    def set_state_bounds(self, state_bounds):
+
+        # compute number of segments
+        self.nseg = len(state_bounds) - 1
+
+        # store bounds
+        self.slb = np.array([s[0] for s in state_bounds], float)
+        self.sub = np.array([s[1] for s in state_bounds], float)
+
+    def set_duration_bounds(self, Tlb, Tub):
+        self.Tlb, self.Tub = Tlb, Tub
 
 class Indirect(Trajectory):
 
@@ -19,9 +30,43 @@ class Indirect(Trajectory):
         # initialise base
         Trajectory.__init__(self, segments)
 
+    def set_state_bounds(self, state_bounds):
+
+        # set state bounds
+        Trajectory.set_state_bounds(self, state_bounds)
+
+        # instantiate segments
+        self.segments = [segment.Indirect(self.dynamics) for i in range(self.nseg)]
+
+    def set_duration_bounds(self, Tlb, Tub):
+        Trajectory.set_duration_bounds(self, Tlb, Tub)
+        if Tlb == Tub:
+            self.freetime = True
+        else:
+            self.freetime = False
+
+    def get_bounds(self):
+        lb = np.hstack((
+            np.full((self.nseg, 1), self.Tlb),
+            self.slb[1:],
+            np.full((self.nseg, self.dynamics.sdim), -10)
+        )).flatten()
+        lb = np.hstack((self.slb[0], lb))
+        ub = np.hstack((
+            np.full((self.nseg, 1), self.Tub),
+            self.sub[1:],
+            np.full((self.nseg, self.dynamics.sdim), 10)
+        )).flatten()
+        ub = np.hstack((self.sub[0], ub))
+        return lb, ub
 
     def fitness(self, z):
 
-        # z = [s00, T0, l00, sf0, ..., TN, l0N, sfN]
+        # [s00, T0, sf0, l00, ..., Tf, sff, l0f]
 
-        z = z.reshape((1 + self.segments[0].dynamics.sdim*2, self.nbc))
+        # extract durations, states, and costates
+        Tl, sl, ll = np.hsplit(z[self.dynamics.sdim:].reshape((self.nseg, 1 + self.dynamics.sdim*2)), [1, 1 + self.dynamics.sdim])
+        Tl = Tl.flatten()
+
+        # set initial state of first segment
+        self.segments[0].set_state
